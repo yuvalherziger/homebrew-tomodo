20b36036df71093099a5b01bca6185754e23e774
Yuvi Herziger <yuvi.herziger@mongodb.com>
2024-01-26 14:14:10 +0200

    First commit

---
+++ Formula/tomodo.rb
@@ -0,0 +1,12 @@
+class Tomodo < Formula
+  desc "A Took for MongoDB on Docker"
+  homepage "https://github.com/yuviherziger/tomodo"
+  url "https://github.com/[your-username]/foo/archive/v1.0.0.tar.gz"
+  sha256 "Your project tar.gz file's SHA256 checksum"
+  license "MIT"
+
+  def install
+    bin.install "tomodo"
+  end
+end
+
