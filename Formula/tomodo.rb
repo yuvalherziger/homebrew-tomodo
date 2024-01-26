041f09d50624ccfff2e3f660348484f016bcddfc
Yuvi Herziger <yuvi.herziger@mongodb.com>
2024-01-26 17:02:10 +0200

    rc.2

--- Formula/tomodo.rb
+++ Formula/tomodo.rb
@@ -1,12 +1,13 @@
 class Tomodo < Formula
   desc "A Tool for MongoDB on Docker"
   homepage "https://github.com/yuviherziger/tomodo"
-  url "https://github.com/yuviherziger/tomodo/releases/download/0.1.0-rc.1/tomodo-0.1.0-rc.1.tar.gz"
-  sha256 "318b8f5cbbc755421bad62c35b5e80112e65b480d69c94f0de34b947100bc7a0"
+  url "https://github.com/yuviherziger/tomodo/releases/download/0.1.0-rc.2/tomodo-0.1.0-rc.2.tar.gz"
+  sha256 "7d26217eddeb0d812710c13f250c6a5aa99e569e05afa212e68963c4b9c6d3dc"
   license "MIT"
 
   def install
     bin.install "tomodo"
+    lib.install Dir["_internal"]
   end
 end
 
