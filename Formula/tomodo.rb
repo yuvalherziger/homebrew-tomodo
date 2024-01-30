dc84614014c21f7863f21e128e6bd240cec2862e
Yuvi Herziger <yuvi.herziger@mongodb.com>
2024-01-30 19:47:28 +0200

    0.1.2

--- Formula/tomodo.rb
+++ Formula/tomodo.rb
@@ -8,7 +8,7 @@ class Tomodo < Formula
     sha256 "ec333231f1c03439d23853043120fc133e59e12264b539823039ad07e1e38477"
   elsif Hardware::CPU.arm?
     url "https://github.com/yuviherziger/tomodo/releases/download/0.1.2/tomodo-arm64-0.1.2.tar.gz"
-    sha256 "2d8d583409925777e830353e08dda90bcd8ebef877acb43f7b1141d0856474f4"
+    sha256 "8c70af7ed1419aef682ff1c06db97b83d9ee9832f8c372ea0a666bac52220d4b"
   end
 
 
