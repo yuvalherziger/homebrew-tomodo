14af155e97bc62f517e8617de1978175b10b1c19
Yuvi Herziger <yuvi.herziger@mongodb.com>
2024-03-12 13:52:24 +0200

    1.0.1

--- Formula/tomodo.rb
+++ Formula/tomodo.rb
@@ -4,11 +4,11 @@ class Tomodo < Formula
   license "MIT"
 
   if Hardware::CPU.intel?
-    url "https://github.com/yuviherziger/tomodo/releases/download/0.1.6/tomodo-amd64-0.1.6.tar.gz"
-    sha256 "513b0921e8a02568aca602b4ddd02501ab8dac58c81cd933f104711f1fe42675"
+    url "https://github.com/yuviherziger/tomodo/releases/download/1.0.1/tomodo-amd64-1.0.1.tar.gz"
+    sha256 "5e11cfeec43cb515a1fd5062ffea70c9f83ab3df1e10ac5cf14b10e9dc9fcf26"
   elsif Hardware::CPU.arm?
-    url "https://github.com/yuviherziger/tomodo/releases/download/0.1.6/tomodo-arm64-0.1.6.tar.gz"
-    sha256 "a1d4ebb3d9fb77e4b3a7343fed33b7b652d19efced0599db5804ca9956b5d7b0"
+    url "https://github.com/yuviherziger/tomodo/releases/download/1.0.1/tomodo-arm64-1.0.1.tar.gz"
+    sha256 "8c2501fc69d02840a4b971fe663b451dc9ab3f78b6192aa4848e37bc1fa6cad3"
   end
 
 
