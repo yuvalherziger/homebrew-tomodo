a4ee37ea2a44c6e5a94ccde5c4958f27779be0b2
Yuvi Herziger <yuvi.herziger@mongodb.com>
2024-03-12 16:26:31 +0200

    Caveats message

--- Formula/tomodo.rb
+++ Formula/tomodo.rb
@@ -18,47 +18,49 @@ class Tomodo < Formula
   end
 
   def caveats
+    red = "\e[32m"
+    green = "\e[92m"
+    yellow = "\e[32m"
+    gray = "\e[37m"
+    teal = "\e[36m"
+    magenta = "\e[94m"
+    blue = "\e[34m"
+    reset = "\033[0m"
+    white = "\e[97m"
+
     <<~EOS
-      Thank you for installing tomodo with Homebrew!
-      
-      -==---:.                                                               
-      .%@%#%@@%#*+-:                                                         
-       :@@=:::-=+#%@%*=:                                                     
-        +@%::::::::-=*%@#=.       .-=++++++++=-::.                           
-        :@@=::---::----=*@@+.   :#@@@%#######%@@@@#*=-=++=:                  
-         %@+:::-==-------=*@%=  #@@@*-------:::=#@@%%@*==*@#-.               
-         #@*:::--=%#-------+%@#.-%@@%++**+====+%@=        :@@%+:             
-         +@#::----=%%=------+%@#.:*@@@@%%@@%%#%@#         +@%#%@#:           
-         -@%--------*@*=----=*%@@@@#+=:::-+*%@@#=:     =#%@%+==*%@*.         
-         .%@+--------=%@*=--+*%#==#%*=-::-+#%*++%@=.  -%@%*+---=+#@%=        
-          =@%=---------*%@#**#@=--+%:=+**+==%===*@@@%%@@@@@#----==*@@+       
-          *@@%+====----==+#%%@@*=-+%----++++%==+#@@@@%*+=*#@@+---==+%@*      
-         =@%%@%++++++*###***#%@++*#%+---++*#@#*+*@@@+:::::-=@@*---=++%@+     
-        .@@+=#@@#+++#@@@%@@@@@@=--=%=*##%#*+@==+*@@@+---=++*@@@#---=+*@@:    
-        *@%--=+%@@#%@#%@:.=#@@@+--+%--::==++%+++#@@@#=--+***@@@@+---=+#@#    
-       .%@+--==+@@@@+ %@:.. .=%@@%#%----****%#%@@%%@@@%##%@@@@=*@#--=++@@-   
-       :@@=-===*@@@*  =@*..... -#@@@@#*+##%@@@@%+--+%@@%@@@@@#..%@=--++%@+   
-       -@@--==+*@@@:   =@#-..... :*@@@@@@@@@%#%@@@%##@@%:--=*..:#@+--++#@*   
-       -@@--==+*@@%   :+.%*..::....-#@@@@@#=:::=#%---=%@#-:::-=*@%++-++#@#   
-       :@@=-=+++@@%   -% **.+#**:::..+@@@@*=+****%++#*%@@@#=-*%@@**+-++%@*   
-       .%@*--+++%@@.   %-:%:+***::::..:#@@*---+**%==++%@@@@--%@@%==-=++@@=   
-        +@%--=++*@@=   -%.=%:.::::::::..=%@%**#%@@@%%@@@@@-:+@@@+---++*@@.   
-        .%@*--=++#@%.   =%:=%=::=+::::::..+#@@@@#+#@@@@@*::=#@@#---=++%@*    
-         -@@=--+++#@%:   :#=.*#=@%::::::::..:=*#%%@%#*=:.:=*@@#=--=++#@%.    
-          -+::--+++#@%=    +#-:*@#:::::::::::::......::--+#@@#=-==++*@@-     
-          .:::-=-=++*%@#:   .+#=%@-:::::::::-----------+*%@@*====++*%@=      
-      :+*%@@@@@@%#**++#%@#=.  ..:%%=:-===----------==+*%@@#=====++#@@=       
-     #@@@#+=:::-=*#@%%#*#%@@#=:  .+%%*+=+=======++**#%@%*=====+++%@%-        
-     :-=+*#%@%#*+-::=+#%@@@@@@@@#*+=+*#%%%%#####%%@@%*+=====+++#%@+.         
-            :-+%@@%*=--:-=+*****##%%@@@@@@@@@@%%#*+=======+++#%@#:           
-                :=*%@%#+=--------=====++++++==========++++*#%@*-             
-                    :+#@@%*+=====================++++++*#%@%+:               
-                       .-+%@@%##**+++++++++++++++**#%%@@#=:                  
-                           .-=*#@@@%%%%#####%%%@@@%#*=:.                     
-                                 .:-==+++++++==-:.                          
-      
-      Remember to ensure Docker Desktop is installed and running!
-      For more information, visit https://github.com/yuviherziger/tomodo
+
+      #{green}Thank you for installing tomodo!#{reset}
+
+      #{red}              #{reset}#{green},#{reset}#{gray}__#{reset}
+      #{red}              #{reset}#{yellow}╣#{reset}#{yellow}▒▒▒▓▓╗#{reset}#{green}▄#{reset}#{gray}_#{reset}
+      #{red}              #{reset}#{green}╘#{reset}#{yellow}▒#{reset}#{yellow}▒▒╩▒▒▒▒▒W#{reset}#{green},#{reset}
+      #{red}               #{reset}#{yellow}▒#{reset}#{yellow}▒▒▒▒╙D▒╠╠╠▒#{reset}#{green}╦        #{reset}#{gray}╒#{reset}#{yellow}▓#{reset}#{yellow}╩╩╩╩DD▓▓#{reset}#{green}H  #{reset}#{blue}_#{reset}#{blue}_ #{reset}#{gray}▄#{reset}#{teal}▄▄#{reset}
+      #{red}               #{reset}#{yellow}D#{reset}#{yellow}▒▒▒▒#{reset}#{green}╕ ╚╠╠╠╠╠▒#{reset}#{green}_       ╙╙#{reset}#{green}'╙#{reset}#{green}╚#{reset}#{green}D[]⌐ #{reset}#{gray}▄#{reset}#{white}█#{reset}#{white}██████#{reset}#{teal}▌#{reset}
+      #{red}               #{reset}#{yellow}║#{reset}#{yellow}▒▒╠╠╠▒ #{reset}#{green}'[╠╠╠╠#{reset}#{green}H,      #{reset}#{gray}_#{reset}#{green}▄#{reset}#{yellow}╗▄#{reset}#{gray}_    #{reset}#{magenta}▄#{reset}#{white}█#{reset}#{white}█▓#{reset}#{teal}█#{reset}#{teal}██▀▀#{reset}#{magenta}" #{reset}#{green}|#{reset}#{green}H_#{reset}
+      #{red}               #{reset}#{green}╘#{reset}#{yellow}╠#{reset}#{yellow}╠╠╠╠╠D#{reset}#{green}╕ #{reset}#{green}╙#{reset}#{yellow}╩#{reset}#{yellow}╠╠#{reset}#{green}H#{reset}#{green}░ #{reset}#{gray},#{reset}#{gray}▄,'#{reset}#{gray}▀#{reset}#{yellow}Φ#{reset}#{yellow}╠▒╠R▀ #{reset}#{gray}_#{reset}#{magenta}▄#{reset}#{green}_ #{reset}#{teal}╙#{reset}#{teal}███▀  #{reset}#{green}╔#{reset}#{green}░#{reset}#{yellow}║#{reset}#{yellow}▒#{reset}#{green}░#{reset}#{green}H#{reset}#{green}_#{reset}
+      #{red}                #{reset}#{green}╚#{reset}#{yellow}╚#{reset}#{yellow}╠╠╠╠╠╠▒#{reset}#{green}╖  #{reset}#{green}²#{reset}#{green}^  #{reset}#{yellow}╠#{reset}#{yellow}D╠ ▀W#{reset}#{gray}▄#{reset}#{gray}▄▄#{reset}#{teal}n#{reset}#{green}▀ ▒#{reset}#{green}░░   #{reset}#{magenta}`     #{reset}#{green}╙#{reset}#{yellow}╣#{reset}#{yellow}╠D#{reset}#{green}▒#{reset}#{green}░╔#{reset}
+      #{red}                 ╚░╙╙╙╚╚╚╙╙░#{reset}#{green}=#{reset}#{green}_  ^#{reset}#{yellow}╙#{reset}#{yellow}╩ ▒╠╠#{reset}#{green}H#{reset}#{green}░░░ M╙#{reset}#{green}'   #{reset}#{gray},#{reset}#{gray}▄#{reset}#{yellow}≡#{reset}#{yellow}▓▓#{reset}#{green}▄#{reset}#{gray}, #{reset}#{yellow}╚#{reset}#{yellow}╠╠▒#{reset}#{green}░#{reset}#{green}╠#{reset}
+      #{red}               µ  #{reset}#{green}'#{reset}#{green}|#{reset}#{green}░░░#{reset}#{green}'   #{reset}#{green}`    #{reset}#{yellow}╠#{reset}#{yellow}D╗ #{reset}#{gray};#{reset}#{green}└#{reset}#{yellow}╙Ü#{reset}#{green}²#{reset}#{green}`, #{reset}#{teal}n#{reset}#{teal}l#{reset}#{green}Ü   #{reset}#{yellow}║#{reset}#{yellow}╠╠R#{reset}#{green}╙#{reset}#{green}├░  ╙#{reset}#{yellow}╠#{reset}#{yellow}╠D#{reset}#{green}░#{reset}#{green}╠#{reset}
+      #{red}              [#{reset}#{yellow}D#{reset}#{green}▒#{reset}#{green}╔#{reset}#{green}_ `²    #{reset}#{teal}▓#{reset}#{teal}▄#{reset}#{magenta},   #{reset}#{yellow}╩#{reset}#{yellow}╠╠ ╠╠▀#{reset}#{gray}*#{reset}#{green}▀#{reset}#{teal}│» ▒░╚   ╚#{reset}#{yellow}D#{reset}#{yellow}╠H#{reset}#{green}░#{reset}#{green}░#{reset}#{green}=   #{reset}#{green}╘#{reset}#{yellow}╠#{reset}#{yellow}╠▒#{reset}#{green}░#{reset}#{green}U#{reset}
+      #{red}              #{reset}#{yellow}╣#{reset}#{yellow}▒#{reset}#{green}░#{reset}#{green}░Ü   #{reset}#{magenta}╓#{reset}#{white}█  #{reset}#{teal}╣#{reset}#{teal}╠║▓╗#{reset}#{magenta},  #{reset}#{gray}' #{reset}#{yellow}╩#{reset}#{yellow}╠╠#{reset}#{green}H#{reset}#{green}░░#{reset}#{green}=    #{reset}#{gray}_#{reset}#{gray},    #{reset}#{green}'     #{reset}#{teal}▌  #{reset}#{yellow}╠#{reset}#{yellow}╠#{reset}#{green}▒#{reset}#{green}░#{reset}
+      #{red}              #{reset}#{yellow}▒#{reset}#{yellow}▒#{reset}#{green}░#{reset}#{green}░#{reset}#{green}H   #{reset}#{white}█#{reset}#{white}█#{reset}#{teal}█ #{reset}#{green}╙╣▒╬╠╣▓#{reset}#{green}▄    #{reset}#{gray}''      #{reset}#{magenta}└#{reset}#{green}╙#{reset}#{yellow}╙Γ   #{reset}#{magenta}▄#{reset}#{blue}_#{reset}#{blue}_  #{reset}#{teal}█#{reset}#{teal}╠H #{reset}#{green}║#{reset}#{yellow}╠H░#{reset}#{green}H#{reset}
+      #{red}              #{reset}#{yellow}D#{reset}#{yellow}▒#{reset}#{green}░#{reset}#{green}░#{reset}#{green}H  #{reset}#{white}╟#{reset}#{white}███#{reset}#{teal}█#{reset}#{magenta}▄ ╣▒D▒╠║▓▄      #{reset}#{gray}╓#{reset}#{yellow}g#{reset}#{yellow}▓╗#{reset}#{green}▄ #{reset}#{gray}╗#{reset}#{yellow}▓#{reset}#{yellow}K#{reset}#{green}⌐ #{reset}#{green}²#{reset}#{teal}╚#{reset}#{teal}╠╠▒R░  #{reset}#{green}░#{reset}#{yellow}╠#{reset}#{yellow}H#{reset}#{green}░#{reset}#{green}r#{reset}
+      #{red}              #{reset}#{yellow}▒#{reset}#{yellow}D#{reset}#{green}░#{reset}#{green}░H  #{reset}#{white}╟#{reset}#{white}██#{reset}#{teal}▌#{reset}#{gray}▐#{reset}#{white}█ #{reset}#{teal}╣#{reset}#{teal}▒#{reset}#{magenta}╓#{reset}#{magenta}▄#{reset}#{teal}╚#{reset}#{teal}▒╠║▓#{reset}#{green}▄    #{reset}#{yellow}╗#{reset}#{gray}▄#{reset}#{green}│#{reset}#{green}▄≈ ╙╚#{reset}#{green}²#{reset}#{green}┐   ²#{reset}#{teal}║#{reset}#{teal}▒#{reset}#{green}^  #{reset}#{green}|#{reset}#{green}░#{reset}#{green}jH░#{reset}#{green}H#{reset}
+      #{red}              #{reset}#{yellow}║#{reset}#{yellow}╠#{reset}#{green}░#{reset}#{green}░░  #{reset}#{teal}▐#{reset}#{white}█▓#{reset}#{white}█ █#{reset}#{gray}▄#{reset}#{green}╙#{reset}#{teal}W#{reset}#{teal}╓╓é▒╠╠╠╣▌#{reset}#{blue}_  #{reset}#{yellow}D#{reset}#{yellow}╠#{reset}#{green}H#{reset}#{green}░#{reset}#{green}= #{reset}#{green}╚#{reset}#{green}Ü¡#{reset}#{green}⌐    #{reset}#{teal}█#{reset}#{teal}░   #{reset}#{green}▒#{reset}#{yellow}D#{reset}#{yellow}╠#{reset}#{green}░#{reset}#{green}░#{reset}
+      #{red}              #{reset}#{green}'#{reset}#{yellow}D#{reset}#{yellow}D#{reset}#{green}░#{reset}#{green}░╔  #{reset}#{white}█#{reset}#{teal}▓█#{reset}#{teal}█ #{reset}#{white}█#{reset}#{magenta}▄#{reset}#{green}╙#{reset}#{teal}D#{reset}#{teal}▒D▒╠╠╠╠╠╣╗#{reset}#{blue}_     #{reset}#{gray},#{reset}#{gray}_     _#{reset}#{white}█#{reset}#{teal}Ü#{reset}#{blue}⌐  #{reset}#{green}[#{reset}#{yellow}╠#{reset}#{yellow}╠#{reset}#{green}D#{reset}#{green}░╚#{reset}
+      #{red}               ╙#{reset}#{yellow}╠#{reset}#{yellow}▒#{reset}#{green}░#{reset}#{green}░#{reset}#{green}╓  #{reset}#{white}█#{reset}#{teal}▓#{reset}#{teal}█#{reset}#{white}█#{reset}#{magenta},#{reset}#{teal}╙#{reset}#{teal}█#{reset}#{blue}`╚▒ [╠╠╠╠╠╠║▓▄#{reset}#{green},      #{reset}#{gray}_#{reset}#{gray}▄#{reset}#{teal}█#{reset}#{teal}▒D#{reset}#{magenta}∩  #{reset}#{green}j#{reset}#{yellow}╠#{reset}#{yellow}╠╠#{reset}#{green}░#{reset}#{green}░#{reset}
+      #{red}                ╙#{reset}#{yellow}╠#{reset}#{yellow}▒#{reset}#{green}░#{reset}#{green}░╔  #{reset}#{white}▀#{reset}#{white}█#{reset}#{teal}▓█#{reset}#{gray}█#{reset}#{magenta}└#{reset}#{teal}▀#{reset}#{gray}█#{reset}#{magenta}. #{reset}#{teal}║#{reset}#{teal}╠╠╠╠╠╠╠╠╠╠╠╠R╠▀▀╚ÜR░#{reset}#{blue}╙  #{reset}#{green}j#{reset}#{yellow}╠#{reset}#{yellow}╠#{reset}#{green}╠#{reset}#{green}░░#{reset}#{green}-#{reset}
+      #{red}                 '#{reset}#{green}╙#{reset}#{yellow}D▒░░#{reset}#{green}_  #{reset}#{white}▀#{reset}#{white}█#{reset}#{teal}▓#{reset}#{teal}██#{reset}#{magenta}▄▓ #{reset}#{blue}²#{reset}#{teal}╠#{reset}#{teal}╠D╠╠╠╠╠╠╠╠╠╠╠╠╠╩░#{reset}#{green}╙  #{reset}#{gray},#{reset}#{yellow}D#{reset}#{yellow}╠╠#{reset}#{green}H#{reset}#{green}░░#{reset}
+      #{red}             #{reset}#{magenta},#{reset}#{magenta}▄#{reset}#{teal}╥#{reset}#{teal}╥#{reset}#{magenta}▄#{reset}#{magenta}▄#{reset}#{blue}_  #{reset}#{green}'#{reset}#{green}╙#{reset}#{green}|.  #{reset}#{gray}^#{reset}#{white}▀#{reset}#{white}█#{reset}#{teal}█#{reset}#{teal}▓█#{reset}#{magenta}▄ ╙DH░░╠╠╠╠╠D╚░░#{reset}#{magenta}=#{reset}#{blue}'  #{reset}#{green},#{reset}#{yellow}1#{reset}#{yellow}╠╠#{reset}#{green}R#{reset}#{green}░░#{reset}#{green}╙#{reset}
+      #{red}                '╙#{reset}#{teal}╙#{reset}#{teal}╚╠K╗#{reset}#{magenta}▄#{reset}#{blue}_       #{reset}#{gray}"#{reset}#{teal}╙#{reset}#{teal}▀▀#{reset}#{magenta}w#{reset}#{magenta}╓#{reset}#{blue}_ #{reset}#{blue}'²#{reset}#{magenta}┌#{reset}#{magenta}:#{reset}#{green}=#{reset}#{blue}=#{reset}#{blue}'`   #{reset}#{green},#{reset}#{green}φ#{reset}#{yellow}D#{reset}#{yellow}╠╠#{reset}#{green}╚#{reset}#{green}Ü░#{reset}#{green}╚#{reset}
+      #{red}                     #{reset}#{blue}'#{reset}#{teal}╙#{reset}#{teal}╚╠╠K╖#{reset}#{green}╔#{reset}#{green}=#{reset}#{green}░░µ╓#{reset}#{green}╓#{reset}#{gray}__       _#{reset}#{gray}_#{reset}#{green}╓#{reset}#{green}╔#{reset}#{green}@#{reset}#{yellow}D#{reset}#{yellow}╠╠╠#{reset}#{green}R#{reset}#{green}╙░░#{reset}#{green}╙#{reset}
+      #{red}                         ^#{reset}#{teal}╙#{reset}#{teal}╚╠░░╚#{reset}#{green}╠#{reset}#{green}╠#{reset}#{yellow}╠#{reset}#{yellow}╠╠╠╠╠╠╠╠╠╠╠╠╠#{reset}#{green}╠#{reset}#{green}╚╙░:░#{reset}#{green}╙#{reset}#{green}`#{reset}
+      #{red}                             `²╚#{reset}#{green}░#{reset}#{green}░░░░░╙╙╙╙╙░░░»░░#{reset}#{green}=#{reset}#{green}²'#{reset}
+      #{red}                                    ```'''```#{reset}
+    
+      #{green}Remember to ensure #{teal}Docker Desktop#{green} is installed and running!#{reset}
+      #{green}For more information, visit https://github.com/yuviherziger/tomodo#{reset}
     EOS
   end
 end
